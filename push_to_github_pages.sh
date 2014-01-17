028919e273608d52063fb388fe9c06c43f331f51
Filip Hracek <filiph@google.com>
2014-01-17 16:28:02 +0100

    Script just makes suggestions, we don't want to fuck anything up

--- push_to_github_pages.sh
+++ push_to_github_pages.sh
@@ -1,1 +1,9 @@
-git push origin gh-pages
+echo 
+echo "Ehm. You should probably do this yourself. Switch to branch gh-pages by:"
+echo "    git checkout gh-pages"
+echo "Then - assuming you were working on master - merge with it:"
+echo "    git merge master"
+echo "And lastly, upload to github:"
+echo "    git push origin gh-pages"
+echo
+echo "                                          " -- Your very helpful \'script\'
